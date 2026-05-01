51bd1c6ed14b12227a0ce1048bcbae1d7c6a723b
Gegi Esitashvili <gegiesitashvili@gmail.com>
2026-05-02 02:11:32 +0400

    update

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,10 +6,10 @@ from .models import InviteCode
 class InviteCodeAdmin(admin.ModelAdmin):
     list_display = ['code', 'is_used', 'used_by', 'created_at']
     list_filter = ['is_used']
-    readonly_fields = ['code', 'is_used', 'used_by', 'created_by', 'created_at']
+    readonly_fields = ['is_used', 'used_by', 'created_by', 'created_at']
 
     def has_add_permission(self, request):
-        return False  # ხელით დამატება დაბლოკილია
+        return True  # ხელით დამატება დაბლოკილია
 
     def has_delete_permission(self, request, obj=None):
-        return False  # წაშლაც დაბლოკილია
+        return True  # წაშლაც დაბლოკილია
