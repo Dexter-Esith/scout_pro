c178067d4bad0f9ae5faccaec33f97531bff28f6
Gegi Esitashvili <gegiesitashvili@gmail.com>
2026-05-02 01:49:12 +0400

    update

--- scout_platform/settings.py
+++ scout_platform/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -52,11 +53,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'scout_platform.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
