c7811b2c3e8f93cf19e04bbc94957731a5c0d4b2
Gegi Esitashvili <gegiesitashvili@gmail.com>
2026-05-02 02:15:10 +0400

    update

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,6 +8,12 @@ class InviteCodeAdmin(admin.ModelAdmin):
     list_filter = ['is_used']
     readonly_fields = ['is_used', 'used_by', 'created_by', 'created_at']
 
+# ეს ფუნქცია ავტომატურად ჩაწერს შემქმნელს (შენ) კოდის შენახვისას
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  # თუ კოდი ახალი იქმნება
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+        
     def has_add_permission(self, request):
         return True  # ხელით დამატება დაბლოკილია
 
